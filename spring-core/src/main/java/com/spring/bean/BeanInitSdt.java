04707a6eb68b2991ca998f6a100b4ee7903c50be
= <=>
2018-09-07 19:29:23 +0800

    add file

--- spring-core/src/main/java/com/spring/bean/BeanInitSdt.java
+++ spring-core/src/main/java/com/spring/bean/BeanInitSdt.java
@@ -1,7 +1,13 @@
 package com.spring.bean;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.SimpleBeanDefinitionRegistry;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.SimpleAliasRegistry;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -9,10 +15,27 @@ public class BeanInitSdt {
 
     public static void main(String[] args) {
 
-        Resource resource = new ClassPathResource("bean.xml");
-        BeanFactory beanFactory = new XmlBeanFactory(resource);
-        Person bean = beanFactory.getBean(Person.class);
-        System.out.println(bean);
+//        Resource resource = new ClassPathResource("bean.xml");
+//        BeanFactory beanFactory = new XmlBeanFactory(resource);
+//        Person bean = (Person) beanFactory.getBean("person");
+//        Student student = (Student) beanFactory.getBean("student");
+//        Teacher teacher = beanFactory.getBean(Teacher.class);
+//        Bar bar = beanFactory.getBean(Bar.class);
+//        System.out.println(bar);
+//        System.out.println(bean+""+student+""+teacher);
+
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
+        Student person = applicationContext.getBean(Student.class);
+        System.out.println(person);
+
+//        DefaultListableBeanFactory defaultListableBeanFactory = new DefaultListableBeanFactory();
+//        SimpleBeanDefinitionRegistry defaultListableBeanFactory = new SimpleBeanDefinitionRegistry();
+//        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(defaultListableBeanFactory);
+//        reader.loadBeanDefinitions(resource);
+//        BeanDefinition person = defaultListableBeanFactory.getBeanDefinition("person");
+//        String factoryBeanName = person.getFactoryBeanName();
+//        System.out.println(factoryBeanName);
+//        System.out.println(person.toString());
     }
 
 }
--- spring-core/src/main/java/com/spring/bean/Person.java
+++ spring-core/src/main/java/com/spring/bean/Person.java
@@ -6,6 +6,16 @@ public class Person {
     private String name;
     private int age;
 
+    private Car car;
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
     public String getName() {
         return name;
     }
